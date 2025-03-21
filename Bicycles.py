afae64f8db41a8b55f2a0820dbe32d12767509ec
Rayz44 <raymondrayz250@gmail.com>
2025-03-21 12:05:03 +0000

    new

--- Bicycles.py
+++ Bicycles.py
@@ -1,1 +1,18 @@
-bicycles = ['trek']
+print ("hello world")
+favorite_language = {
+    'jen':'python',
+    'sarah' : 'c',
+    'edward' : 'rust',
+    'phil' : 'python',
+}
+print (favorite_language)
+#more examples
+favorite_language = {
+    'jen' :'python',
+    'sarah' : 'c',
+    'edward' : 'rust',
+    'phil' : 'python',
+}
+
+for name, language in favorite_language.items():
+    print (f"{name.title()}'s favorite language is {language.upper()}")
