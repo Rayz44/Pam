7f04a0f8eebae41b744d375f0f3a67d7ae3c165c
Rayz44 <raymondrayz719@gmail.com>
2025-03-21 09:48:11 +0300

    Added changes to file

---
+++ Indentation test.py
@@ -0,0 +1,6 @@
+items_on_menu = ('rice', 'chiken nuggets', 'mashed potatoes', 'steamed carrots', 'plain pasta')
+print (f"I would like to order 1 plate of, {items_on_menu[-3].title()}")
+customer_2 = f"I want a plate of {items_on_menu[0]} plus some {items_on_menu[1]}"
+print (customer_2)
+customer_3 = f"please i need a bowl of {items_on_menu[2].title()}"
+print (customer_3)
